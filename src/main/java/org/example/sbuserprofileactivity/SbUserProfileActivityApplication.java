a416a6ea55e31276946cf09fcbe484ce193cfaab
John Joseph P. Asoro <139239358+xEN-JJ@users.noreply.github.com>
2024-12-01 15:33:41 +0800

    init commit

---
+++ src/main/java/org/example/sbuserprofileactivity/SbUserProfileActivityApplication.java
@@ -0,0 +1,12 @@
+package org.example.sbuserprofileactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbUserProfileActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbUserProfileActivityApplication.class, args);
+	}
+}
---
+++ src/test/java/org/example/sbuserprofileactivity/SbUserProfileActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sbuserprofileactivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbUserProfileActivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
