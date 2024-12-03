edc5830b5ad002ff5e74ba8da7ce98e48fd9bc9b
John Joseph P. Asoro <139239358+xEN-JJ@users.noreply.github.com>
2024-12-03 23:46:24 +0800

    First commit

---
+++ src/main/java/org/example/sbuserprofileactivity/student/Student.java
@@ -0,0 +1,166 @@
+package org.example.sbuserprofileactivity.student;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private Long id;
+    private String name;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String gender;
+    private Integer age;
+    private String address;
+    private LocalDate birthdate;
+    private String profileImageUrl;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, String firstName, String lastName, String email, String password, String gender, Integer age, String address, LocalDate birthdate, String profileImageUrl) {
+        this.id = id;
+        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.age = age;
+        this.address = address;
+        this.birthdate = birthdate;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Student(String name, String firstName, String lastName, String email, String password, String gender, Integer age, String address, LocalDate birthdate, String profileImageUrl) {
+        this.name = name;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.age = age;
+        this.address = address;
+        this.birthdate = birthdate;
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                ", birthdate=" + birthdate +
+                ", profileImageUrl='" + profileImageUrl + '\'' +
+                '}';
+    }
+}
